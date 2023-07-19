752b0a45011338fb0b19eec1c06e9f77653c2d4e
bewe <16521486@mahasiswa.itb.ac.id>
2023-07-19 21:30:44 +0700

    feat: read file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,48 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
 const App: React.FC = () => {
-  const [result, setResult] = useState<string>('')
+  const [result, setResult] = useState<string>('');
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
 
   useEffect(() => {
-    async function fetchData (): Promise<void> {
+    async function fetchData(): Promise<void> {
       try {
-        const response = await fetch('http://localhost:8080')
-        const data = await response.text()
-        setResult(data)
+        if (selectedFile) {
+          const formData = new FormData();
+          formData.append('file', selectedFile);
+
+          const response = await fetch('http://localhost:8080', {
+            method: 'POST',
+            body: formData,
+          });
+
+          const data = await response.json();
+          setResult(JSON.stringify(data, null, 2));
+        }
       } catch (error) {
-        console.error(error)
+        console.error(error);
       }
     }
 
-    fetchData()
-      .catch((error) => { console.error(error) }
-      )
-  }, [])
+    fetchData().catch((error) => {
+      console.error(error);
+    });
+  }, [selectedFile]);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files && event.target.files[0];
+    setSelectedFile(file);
+  };
 
   return (
     <div className="flex justify-center items-center h-screen bg-gray-100">
       <div className="bg-white p-8 rounded shadow">
         <h1 className="text-2xl font-bold mb-4">Graph Results</h1>
-        <div className="whitespace-pre-line">{result}</div>
+        <input type="file" accept=".txt" onChange={handleFileChange} />
+        <pre>{result}</pre>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
