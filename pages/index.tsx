7e0e1ab7744d236d9c208df30f356c25e6704bd3
bewe <16521486@mahasiswa.itb.ac.id>
2023-07-27 22:05:52 +0700

    feat: visual scc

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import Graph from 'react-graph-vis'
+import Graph from 'react-graph-vis';
 
 const App: React.FC = () => {
   const [result, setResult] = useState<string>('');
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [graphData, setGraphData] = useState<any>({ nodes: [], edges: [] });
   const [graphBridge, setGraphBridge] = useState<any>({ nodes: [], edges: [] });
+  const [graphScc, setGraphScc] = useState<any>({ nodes: [], edges: [] });
 
   useEffect(() => {
     async function fetchData(): Promise<void> {
@@ -50,11 +51,39 @@ const App: React.FC = () => {
             }
           }
 
-          const nodesData = nodes.map((node) => ({ id: node, label: node }));
-          setGraphData({ nodes: nodesData, edges });
+          // Extract nodes and edges from the "scc" array
+          const { scc } = data;
+          const sccNodes = [];
+          const sccEdges = [];
 
-          const nodesBridgeData = bridgeNodes.map((node) => ({ id: node, label: node }));
-          setGraphBridge({ nodes: nodesBridgeData, edges: bridgeEdges });
+          for (const edge of scc) {
+            const [from, to] = edge.match(/[A-Z]/g) || [];
+            if (from && to) {
+              sccEdges.push({ from, to });
+              if (!sccNodes.includes(from)) sccNodes.push(from);
+              if (!sccNodes.includes(to)) sccNodes.push(to);
+            }
+          }
+
+          // Gather all unique nodes from the "scc" result
+          const sccNodesSet = new Set(scc.map((edge) => edge.split('')));
+          const sccNodesUnique = Array.from(sccNodesSet).flat();
+
+          // Find the nodes in "scc" that are not present in the "graph" result
+          const nodesWithoutEdges = sccNodesUnique.filter((node) => !nodes.includes(node));
+
+          // Add nodes without edges to the graph data
+          const allNodesData = nodes.map((node) => ({ id: node, label: node }));
+          const nodesWithoutEdgesData = nodesWithoutEdges.map((node) => ({
+            id: node,
+            label: node,
+          }));
+
+          // Set the graph data including nodes without edges
+          setGraphData({ nodes: allNodesData, edges });
+          setGraphBridge({ nodes: bridgeNodes.map((node) => ({ id: node, label: node })), edges: bridgeEdges });
+          setGraphScc({ nodes: allNodesData.concat(nodesWithoutEdgesData), edges: sccEdges });
+
         }
       } catch (error) {
         console.error(error);
@@ -88,17 +117,37 @@ const App: React.FC = () => {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
+    <div className="flex justify-center items-center h-auto bg-gray-100">
       <div className="bg-white p-8 rounded shadow">
         <h1 className="text-2xl font-bold mb-4">Graph Results</h1>
         <input type="file" accept=".txt" onChange={handleFileChange} />
-        <pre>{result}</pre>
-        {/* {graphData.nodes.length > 0 && (
-          <Graph graph={graphData} options={options} events={events} />
-        )}
-        {graphBridge.nodes.length > 0 && (
-          <Graph graph={graphBridge} options={options} events={events} />
-        )} */}
+        {/* <pre>{result}</pre> */}
+        <div style={{ display: 'flex' }}>
+          <div style={{ flex: 1 }}>
+            {graphData.nodes.length > 0 && (
+              <div>
+                <h2>Graph</h2>
+                <Graph graph={graphData} options={options} events={events} />
+              </div>
+            )}
+          </div>
+          <div style={{ flex: 1 }}>
+            {graphBridge.nodes.length > 0 && (
+              <div>
+                <h2>Bridges</h2>
+                <Graph graph={graphBridge} options={options} events={events} />
+              </div>
+            )}
+          </div>
+          <div style={{ flex: 1 }}>
+            {graphScc.nodes.length > 0 && (
+              <div>
+                <h2>SCC</h2>
+                <Graph graph={graphScc} options={options} events={events} />
+              </div>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
